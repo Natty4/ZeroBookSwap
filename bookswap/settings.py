0f7055610e92adbb08047efb6a024a98712ed4c4
natty4 <natty7kt@gmail.com>
2025-11-28 22:45:10 +0300

    Add trusted origin for CSRF protection in settings

--- bookswap/settings.py
+++ bookswap/settings.py
@@ -151,6 +151,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8001",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://zero-com.netlify.app/",
 ]
 
 # Cookie settings for development
